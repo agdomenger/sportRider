e830f1d4925c8a1e80219fe114052bf63e55d60b
DOMENGER Agathe <agdomenger@ensc.fr>
2024-02-20 16:54:07 +0100

    API OK + verif de la connexion utilisateur OK

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
-import 'signup.dart';
+import 'package:http/http.dart' as http; // Importer la bibliothèque HTTP
+import 'dart:convert';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String authMessage = ''; // Message d'authentification à afficher
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +23,7 @@ class LoginPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             TextField(
+              controller: emailController,
               decoration: InputDecoration(
                 labelText: 'Email',
                 focusedBorder: UnderlineInputBorder(
@@ -20,12 +31,14 @@ class LoginPage extends StatelessWidget {
                       BorderSide(color: Theme.of(context).primaryColorDark),
                 ),
                 labelStyle: TextStyle(
-                    color: Theme.of(context).primaryColorDark,
-                    fontWeight: FontWeight.bold),
+                  color: Theme.of(context).primaryColorDark,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
             SizedBox(height: 16.0),
             TextField(
+              controller: passwordController,
               obscureText: true,
               decoration: InputDecoration(
                 labelText: 'Password',
@@ -34,38 +47,67 @@ class LoginPage extends StatelessWidget {
                       BorderSide(color: Theme.of(context).primaryColorDark),
                 ),
                 labelStyle: TextStyle(
-                    color: Theme.of(context).primaryColorDark,
-                    fontWeight: FontWeight.bold),
+                  color: Theme.of(context).primaryColorDark,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
             SizedBox(height: 16.0),
             ElevatedButton(
-              onPressed: () {
-                // Placeholder for login logic
-                print('Login Button Pressed');
+              onPressed: () async {
+                // Utilisez la bibliothèque http pour envoyer une requête à votre API d'authentification
+                final response = await http.get(
+                  Uri.parse(
+                      'http://localhost:8080/connect?email=${emailController.text}&password=${passwordController.text}'),
+                  headers: <String, String>{
+                    'Content-Type': 'application/json',
+                  },
+                );
+
+                if (response.statusCode == 200) {
+                  // Authentification réussie
+                  final email = emailController.text;
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Bonjour $email'),
+                        content: Text('Vous êtes connecté!'),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                } else {
+                  // Authentification échouée
+                  setState(() {
+                    authMessage = 'Authentication failed. Please try again.';
+                  });
+                }
               },
               child: Text('Login'),
               style: ElevatedButton.styleFrom(
-                  foregroundColor: Theme.of(context).primaryColor,
-                  backgroundColor: Theme.of(context).primaryColorDark),
-            ),
-            SizedBox(height: 16.0),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SignupPage(
-                      backgroundColor: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                );
-              },
-              child: Text('Don\'t have an account? Sign up'),
-              style: TextButton.styleFrom(
-                foregroundColor: Theme.of(context).primaryColorDark,
+                foregroundColor: Theme.of(context).primaryColor,
+                backgroundColor: Theme.of(context).primaryColorDark,
               ),
             ),
+            SizedBox(height: 16.0),
+            Text(
+              authMessage,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            // ... (votre code pour le bouton Sign up)
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:sport_rider/Pages/Bottom.dart';
 import 'package:sport_rider/Pages/profil.dart';
 import 'themes.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'Pages/Login.dart';
 
 void main() {
@@ -40,9 +42,51 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).primaryColor,
       ),
       body: Center(
-        child: ProfilePage(),
+        child: LoginPage(),
       ),
       //bottomNavigationBar: MyBottomNavigationBar(), il faut que ce soit seulement pour les pages une fois connecté
+    );
+  }
+}
+
+Future<List<dynamic>> fetchComptes() async {
+  final response = await http.get(Uri.parse('http://localhost:8080/comptes'));
+
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw Exception('Failed to load comptes');
+  }
+}
+
+class ComptesWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Liste des comptes'),
+      ),
+      body: FutureBuilder<List<dynamic>>(
+        future: fetchComptes(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Erreur : ${snapshot.error}');
+          } else {
+            final comptes = snapshot.data!;
+            return ListView.builder(
+              itemCount: comptes.length,
+              itemBuilder: (context, index) {
+                final compte = comptes[index];
+                return ListTile(
+                  title: Text(compte['email']),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
